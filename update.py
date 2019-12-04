ed389cb4d20d42cab81ca147ffe8b32614ee7bf6
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-12-04 15:12:05 -0500

    Only process the assignments if the tool was found to delete, otherwise there's nothing to update.

--- update.py
+++ update.py
@@ -28,22 +28,24 @@ for site in CONFIG.get("SITES_TO_UPDATE"):
     # Find and delete the existing LTI tool
     logger.info ("Searching for M-Write tool")
     tools = course.get_external_tools()
+    tool_found = False
     for tool in tools:
         if "M-Write" in tool.name:
             logger.info(f"Found tool named {tool.name}, deleting")
             tool.delete()
             logger.info("Creating new M-Write Tool")
-            course.create_external_tool("M-Write Peer Review", "anonymous",
-                LTI_KEY, LTI_SECRET, config_xml=LTI_XML, config_type="by_xml")
-    # Re-add the new tool
-    course.create_external_tool("M-Write Peer Review", "anonymous",
-        LTI_KEY, LTI_SECRET, config_xml=LTI_XML, config_type="by_xml")
-    # Get all the assignments and relink lti onesthem
-    assignments = course.get_assignments()
-    for assignment in assignments:
-        if hasattr(assignment, 'external_tool_tag_attributes') and \
-            'mwrite' in assignment.external_tool_tag_attributes.get('url'):
-            logger.info(f"Found LTI Assignment {assignment.name}, updating external URL")
-            # TODO: This could be pulled out of the XML
-            assignment.edit(assignment={'external_tool_tag_attributes': 
-                {'url': CONFIG.get("LTI_NEW_LAUNCH")}})
+            # Re-add the new tool
+            tool_found = True
+    # If we found an LTI tool    
+    if tool_found:
+        course.create_external_tool("M-Write Peer Review", "anonymous",
+            LTI_KEY, LTI_SECRET, config_xml=LTI_XML, config_type="by_xml")
+        # Get all the assignments and relink lti onesthem
+        assignments = course.get_assignments()
+        for assignment in assignments:
+            if hasattr(assignment, 'external_tool_tag_attributes') and \
+                'mwrite' in assignment.external_tool_tag_attributes.get('url'):
+                logger.info(f"Found LTI Assignment {assignment.name}, updating external URL")
+                # TODO: This could be pulled out of the XML
+                assignment.edit(assignment={'external_tool_tag_attributes': 
+                    {'url': CONFIG.get("LTI_NEW_LAUNCH")}})
